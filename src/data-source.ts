c471f10c76faad9e2c7801ab0d274b8391cd2961
GuilhermeAndreotti <gandreotti@alunos.utfpr.edu.br>
2024-03-13 20:03:46 -0300

    feat: adding connection with database

---
+++ src/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { SeederOptions } from 'typeorm-extension';
+
+dotenv.config();
+
+export const dataSourceOptions: DataSourceOptions & SeederOptions = {
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: +process.env.TYPEORM_PORT || 5432,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [`${__dirname}/**/models/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+  seeds: [],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
